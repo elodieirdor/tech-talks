59f4a8f1b546a5eb4713b08a010adbb10ecd702a
Elodie <elodie.irdor@gmail.com>
2020-09-04 21:04:10 +1200

    Small fixes

--- src/resources/js/components/App.vue
+++ src/resources/js/components/App.vue
@@ -16,3 +16,16 @@ export default {
   },
 };
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.main-container{
+  margin-top: 20px;
+}
+</style>
