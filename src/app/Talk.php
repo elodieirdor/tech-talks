7a9f029bc2d26de9b87f7eb4a7b02fa038d5ff53
Elodie <elodie.irdor@gmail.com>
2020-09-04 21:50:50 +1200

    Add passport for authentication and talk model

---
+++ src/app/Talk.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Talk extends Model
+{
+    const STATUS_PUBLISHED = 'published';
+    const STATUS_DRAFT = 'draft';
+    const STATUS = [self::STATUS_PUBLISHED, self::STATUS_DRAFT];
+
+    protected $fillable = [
+        'topic', 'description', 'date', 'status'
+    ];
+}
---
+++ src/database/migrations/2020_09_04_092558_create_talks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Talk;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTalksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('talks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('topic', 80);
+            $table->text('description');
+            $table->date('date');
+            $table->enum('status', Talk::STATUS);
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('talks');
+    }
+}
