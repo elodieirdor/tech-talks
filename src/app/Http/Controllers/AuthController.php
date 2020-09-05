eadf8921cb264b0d3aac061d78a02991926cbb87
Elodie <elodie.irdor@gmail.com>
2020-09-06 04:39:23 +1200

    Add API route for login

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Factory\Response\AuthResponseFactory;
 use App\Factory\Model\UserFactory;
 use App\Http\Requests\StoreUser;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -12,6 +13,24 @@ class AuthController extends Controller
     {
         $user = $userFactory->createFromRequest($request);
         $authResponse = $authResponseFactory->createFromUser($user);
+
+        return response()->json($authResponse->toArray(), 200);
+    }
+
+    public function login(Request $request, AuthResponseFactory $authResponseFactory)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        $successAuth = auth()->attempt($data);
+
+        if (false === $successAuth) {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+
+        $authResponse = $authResponseFactory->createFromUser(auth()->user());
 
         return response()->json($authResponse->toArray(), 200);
     }
