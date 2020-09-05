809bb1059e539b33722a3447ac41d5c33cc03d97
Elodie <elodie.irdor@gmail.com>
2020-09-06 04:03:23 +1200

    Publish a talk

--- src/resources/js/components/talk/TalkList.vue
+++ src/resources/js/components/talk/TalkList.vue
@@ -12,14 +12,17 @@
                     :to="{ name: 'edit_talk', params: { id: row.item.id }}"
                     tag="button"
                     class="btn mr-1 btn-secondary btn-sm"
+                    v-if="row.item.status !== 'published'"
                 >Edit
                 </router-link>
-                <router-link
-                    :to="{ name: 'edit', params: { id: row.item.id }}"
-                    tag="button"
+                <button
+                    type="button"
                     class="btn mr-1 btn-success btn-sm"
+                    @click="publish(row.item.id, row.index)"
+                    v-if="row.item.status !== 'published'"
                 >Publish
-                </router-link>
+                </button>
+                <span v-else>Published</span>
             </template>
         </b-table>
     </div>
@@ -33,6 +36,23 @@ export default {
         return {
             fields: this.canEdit ? fields.concat(['actions']) : fields,
         };
+    },
+    methods: {
+        publish(id, index) {
+            const config = {
+                headers: {Authorization: `Bearer ${this.$store.state.token}`}
+            }
+            window.axios
+                .put(`/api/talks/${id}`, {}, config)
+                .then(response => {
+                    this.talks.splice(index, 1, response.data.data)
+                })
+                .catch(error => {
+                    if (error.response) {
+                        this.errors = error.response.data.errors;
+                    }
+                })
+        }
     },
     props: {
         talks: {
